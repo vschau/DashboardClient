4bb13cea9a16a153d4b5497201e1e16ae1584fd3
Vanessa Chau <vanessaschau@gmail.com>
2020-02-16 02:21:41 -0800

    Fix layout

--- src/app/sessions/session-sales/session-sales.component.ts
+++ src/app/sessions/session-sales/session-sales.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./session-sales.component.css']
 })
 export class SessionSalesComponent implements OnInit {
-  calc2Cols = '2 2 calc(10em + 10px);';
-  /** 10px is the missing margin of the missing box */
-  calc3Cols = '3 3 calc(15em + 20px)';
-  /** 20px is the missing margin of the two missing boxes */
+
   constructor() { }
 
   ngOnInit() {
